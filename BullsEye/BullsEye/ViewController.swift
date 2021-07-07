445aa134a65fb755192fb06e4d47591049ce6394
datmcl <mixail.bokov9@gmail.com>
2021-07-07 22:02:47 +0700

    add getting slider value

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -9,18 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var currentValue: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hello world!", message: "My first app!", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Current value of sldier", message: "\(currentValue)", preferredStyle: .alert)
         
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
-
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = Int(slider.value)
+    }
 }
 
