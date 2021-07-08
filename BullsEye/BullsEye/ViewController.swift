37e8d81b9e37813b03c28ae2f1145b18f420161d
datmcl <mixail.bokov9@gmail.com>
2021-07-08 23:09:27 +0700

    add target value display

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -13,11 +13,11 @@ class ViewController: UIViewController {
     var targetValue: Int = 0
     
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        slider.value = Float(currentValue)
-        targetValue = Int.random(in: 1...100)
+        startNewRound()
     }
     @IBAction func showAlert() {
         let message = "The value of the slider is \(currentValue)\nThe target value is \(targetValue)"
@@ -25,10 +25,17 @@ class ViewController: UIViewController {
         let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
         currentValue = Int(slider.value)
     }
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        targetLabel.text = "\(targetValue)"
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
 }
-
