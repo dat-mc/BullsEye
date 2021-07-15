a4d0fc489c52c76157245d1a812e8a63c01e6296
datmcl <mixail.bokov9@gmail.com>
2021-07-15 15:52:29 +0700

    add info screen

---
+++ BullsEye/BullsEye/AboutViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by datmcl on 7/15/21.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
